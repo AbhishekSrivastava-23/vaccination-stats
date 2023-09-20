53da598494555e86b9b00c246ea2f339d212b4a8
Abhishek Srivastava <abhishek232004@gmail.com>
2023-09-21 01:02:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typedb.client import *
 def highestVaccines():
 
     with TypeDB.core_client("localhost:1729") as client:
-        with client.session("vaccination_schema", SessionType.DATA) as session:
+        with client.session("vaccination", SessionType.DATA) as session:
             with session.transaction(TransactionType.READ) as read_transaction:
 
                 answer_iterator = read_transaction.query().match_group_aggregate('match $p isa person, has country $c; get $p, $c; group $c; count;')
@@ -17,25 +17,25 @@ def highestVaccines():
 
                 print("\nTop 3 countries with most Vaccinations:")
                 for i in range(3):
-                    print(stats[i][1], " : ", stats[i][0])
+                    print(stats[i][1], ": ", stats[i][0])
 
-def vaccineByAge(l, r):
+def vaccineByAge(lower_limit, upper_limit):
     
     with TypeDB.core_client("localhost:1729") as client:
-        with client.session("vaccination_schema", SessionType.DATA) as session:
+        with client.session("vaccination", SessionType.DATA) as session:
             with session.transaction(TransactionType.READ) as read_transaction:
 
-                answer_iterator = read_transaction.query().match_group_aggregate('match $p isa person, has age >= ' + str(l) + ', has age <= ' + str(r) + '; $m isa manufacturer, has vaccine_name $v; $vcc (person: $p, manufacturer: $m) isa vaccinator; get $p, $v; group $v; count;')
+                answer_iterator = read_transaction.query().match_group_aggregate(f'match $p isa person, has age >= {lower_limit}, has age <= {upper_limit}; $m isa manufacturer, has vaccine_name $v; $vcc (person: $p, manufacturer: $m) isa vaccinator; get $p, $v; group $v; count;')
 
-                print("\nVaccines given to age group", l, '-', r, "years:")
+                print(f"\nVaccines given to age group {lower_limit} - {upper_limit} years:")
 
                 for answer in answer_iterator:
-                    print(answer._owner._value, " : ", answer._numeric._int_value)
+                    print(answer._owner._value, ": ", answer._numeric._int_value)
 
 def vaccineByCountry(country):
 
     with TypeDB.core_client("localhost:1729") as client:
-        with client.session("vaccination_schema", SessionType.DATA) as session:
+        with client.session("vaccination", SessionType.DATA) as session:
             with session.transaction(TransactionType.READ) as read_transaction:
 
                 country_iterator = read_transaction.query().match('match $p isa person, has country $c; get $c;')
@@ -46,19 +46,19 @@ def vaccineByCountry(country):
                     countries.append(name.get_value())
 
                 if country not in countries:
-                    print("Country not found")
+                    print("No entries for the given country!")
                     return
 
-                answer_iterator = read_transaction.query().match_group_aggregate('match $p isa person, has country = "' + country + '"; $m isa manufacturer, has vaccine_name $v; $vcc (person: $p, manufacturer: $m) isa vaccinator; get $p, $v; group $v; count;')
+                answer_iterator = read_transaction.query().match_group_aggregate(f'match $p isa person, has country = "{country}"; $m isa manufacturer, has vaccine_name $v; $vcc (person: $p, manufacturer: $m) isa vaccinator; get $p, $v; group $v; count;')
 
-                print("\nVaccines provided in", country, ":")
+                print(f"\nVaccines provided in {country}:")
                 for answer in answer_iterator:
-                    print(answer._owner._value, " : ", answer._numeric._int_value)
+                    print(answer._owner._value, ": ", answer._numeric._int_value)
 
 def avgAgeByCountry():
 
     with TypeDB.core_client("localhost:1729") as client:
-        with client.session("vaccination_schema", SessionType.DATA) as session:
+        with client.session("vaccination", SessionType.DATA) as session:
             with session.transaction(TransactionType.READ) as read_transaction:
 
                 answer_iterator = read_transaction.query().match_group_aggregate('match $p isa person, has country $c, has age $a; get $c, $a; group $c; mean $a;')
@@ -66,13 +66,13 @@ def avgAgeByCountry():
                 print("\nAverage age of people vaccinated in each country:\n")
 
                 for answer in answer_iterator:
-                    print(answer._owner._value, " : ", round(answer._numeric._float_value, 1), "years")
+                    print(answer._owner._value, ": ", round(answer._numeric._float_value), "years")
     
 
 while (True):
 
     print("\nMENU")
-    print("1. Display countries with most vaccinations")
+    print("1. Display top 3 countries with most vaccinations")
     print("2. Display vaccine-wise stats for a given age group")
     print("3. Display vaccine-wise stats for a given country")
     print("4. Display average age of people vaccinated in each country")
@@ -83,12 +83,15 @@ while (True):
         highestVaccines()
 
     elif (choice == '2'):
-        l = int(input("\nEnter lower limit: "))
-        r = int(input("Enter upper limit: "))
-        if (l > r):
+        try:
+            lower_lim = int(input("\nEnter lower limit: "))
+            upper_lim = int(input("Enter upper limit: "))
+            if (lower_lim > upper_lim):
+                print("Invalid Input")
+                continue
+            vaccineByAge(lower_lim, upper_lim)
+        except:
             print("Invalid Input")
-            continue
-        vaccineByAge(l, r)
 
     elif (choice == '3'):
         country = input("\nEnter country name: ")
